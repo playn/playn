205eed0afe7cb9f5808cdc5ee8fa955a8b7e908d
Fred Sauer <fredsa@google.com>
2011-05-20 19:22:29 +0000

    log().debug() whether we're using Flash vs. HTML5 audio

--- core/src/forplay/html/HtmlAudio.java
+++ core/src/forplay/html/HtmlAudio.java
@@ -1,21 +1,20 @@
 /**
  * Copyright 2010 The ForPlay Authors
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package forplay.html;
 
 import com.allen_sauer.gwt.voices.client.FlashSound;
+import com.allen_sauer.gwt.voices.client.Sound;
 import com.allen_sauer.gwt.voices.client.SoundController;
 import com.allen_sauer.gwt.voices.client.ui.FlashMovie;
 
@@ -23,28 +22,33 @@ import forplay.core.Audio;
 import forplay.core.ForPlay;
 
 /**
- * This class is temporarily public, in order to expose
- * {@link #isFlash9AudioPluginMissing()}, to assist with in game Flash detection.
+ * This class is temporarily public, in order to expose {@link #isFlash9AudioPluginMissing()}, to
+ * assist with in game Flash detection.
  */
 public class HtmlAudio implements Audio {
 
   private SoundController soundController;
 
+  @SuppressWarnings("deprecation")
   public HtmlAudio() {
     soundController = new SoundController();
+    ForPlay.log().debug(
+        "Preferred sound type: " + soundController.getPreferredSoundType().getName());
   }
 
   HtmlSound createSound(String url) {
-    return new HtmlSound(soundController.createSound("audio/mpeg", url));
+    Sound sound = soundController.createSound("audio/mpeg", url);
+    // ForPlay.log().debug(sound.getClass().getName() + " " + sound.getUrl());
+    return new HtmlSound(sound);
   }
 
   @SuppressWarnings("deprecation")
   public boolean isFlash9AudioPluginMissing() {
     if (soundController.getPreferredSoundType() != FlashSound.class) {
-      // HTML5 audio requested; skip Flash check
+      ForPlay.log().debug("HTML5 audio requested; skipping Flash check");
       return false;
     }
-    
+
     ForPlay.log().debug(
         "FlashMovie.isExternalInterfaceSupported: " + FlashMovie.isExternalInterfaceSupported());
     ForPlay.log().debug("FlashMovie.getMajorVersion: " + FlashMovie.getMajorVersion());
@@ -52,7 +56,7 @@ public class HtmlAudio implements Audio {
       // Flash plugin operational and is at least version 9
       return false;
     }
-    
+
     // Audio missing or disabled
     return true;
   }
