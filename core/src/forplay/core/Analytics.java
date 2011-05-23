94b8b765d325bae62f88cb24edbfcca0c1787eac
Fred Sauer <fredsa@google.com>
2011-05-23 05:43:10 +0000

    Introduce Analytics.Category class which represents the (float sampleRate, String category) pair

--- core/src/forplay/core/Analytics.java
+++ core/src/forplay/core/Analytics.java
@@ -1,56 +1,82 @@
 /**
  * Copyright 2011 The ForPlay Authors
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 
 package forplay.core;
 
 public interface Analytics {
-  public abstract class AnalyticsImpl implements Analytics {
-    @Override
-    public void logEvent(float sampleRate, String category, String action) {
-      ForPlay.log().debug(
-          "Analytics#logEvent(" + sampleRate + ", " + category + ", " + action + ")");
+  public static class Category {
+    private final float sampleRate;
+    private final String category;
+
+    /**
+     * @param sampleRate likelihood that this event should be logged during this
+     *          game session, range {@literal [0.0 - 1.0)}
+     * @param category the general event category, e.g. {@literal "Videos"}
+     */
+    public Category(float sampleRate, String category) {
+      this.sampleRate = sampleRate;
+      this.category = category;
     }
 
     @Override
-    public void logEvent(float sampleRate, String category, String action, String label, int value) {
+    public String toString() {
+      return getCategory() + "[rate=" + getSampleRate() + "]";
+    }
+
+    public float getSampleRate() {
+      return sampleRate;
+    }
+
+    public String getCategory() {
+      return category;
+    }
+  }
+
+  public abstract class AnalyticsImpl implements Analytics {
+    @Override
+    public void logEvent(Category category, String action) {
+      ForPlay.log().debug("Analytics#logEvent(category=" + category + ", action=" + action + ")");
+    }
+
+    @Override
+    public void logEvent(Category category, String action, String label, int value) {
       ForPlay.log().debug(
-          "Analytics#logEvent(" + sampleRate + ", " + category + ", " + action + ", " + label
-              + ", " + value + ")");
+          "Analytics#logEvent(category=" + category + ", action=" + action + ", label=" + label
+              + ", value=" + value + ")");
     }
   }
 
   /**
    * Log an event with a given sampleRate, range {@literal [0.0 - 1.0)}.
    * 
-   * @param sampleRate likelihood that this event should be logged during this game session, range
-   *          {@literal [0.0 - 1.0)}
-   * @param category the general event category, e.g. {@literal "Videos"}
+   * @param category the analytics {@link Category}
    * @param action the action for the event, e.g. {@literal "Play"}
    */
-  void logEvent(float sampleRate, String category, String action);
+  void logEvent(Category category, String action);
 
   /**
    * Log an event with a given sampleRate, range {@literal [0.0 - 1.0)}.
    * 
-   * @param sampleRate likelihood that this event should be logged during this game session, range
-   *          {@literal [0.0 - 1.0)}
-   * @param category the general event category, e.g. {@literal "Videos"}
+   * @param category the analytics {@link Category}
    * @param action the action for the event, e.g. {@literal "Play"}
    * @param label descriptor for the event
-   * @param value value associated with the event. You can see your event values in the Overview,
-   *          Categories, and Actions reports, where they are listed by event or aggregated across
-   *          events, depending upon your report view
+   * @param value value associated with the event. You can see your event values
+   *          in the Overview, Categories, and Actions reports, where they are
+   *          listed by event or aggregated across events, depending upon your
+   *          report view
    */
-  void logEvent(float sampleRate, String category, String action, String label, int value);
+  void logEvent(Category category, String action, String label, int value);
 }
--- core/src/forplay/html/HtmlAnalytics.java
+++ core/src/forplay/html/HtmlAnalytics.java
@@ -1,69 +1,82 @@
 /**
  * Copyright 2011 The ForPlay Authors
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 package forplay.html;
 
-import static forplay.core.ForPlay.*;
+import static forplay.core.ForPlay.log;
+
+import com.google.gwt.user.client.Window;
+
 import forplay.core.Analytics;
 import forplay.core.ForPlay;
 
 public class HtmlAnalytics implements Analytics {
 
   /**
-   * Random value, determined once per game / session, so that multiple events with the same sample
-   * rate will be consistently logged -or- not logged in any given session. We don't use Google
-   * Analytics' <a href=
+   * Random value, determined once per game / session, so that multiple events
+   * with the same sample rate will be consistently logged -or- not logged in
+   * any given session. We don't use Google Analytics' <a href=
    * "http://code.google.com/apis/analytics/docs/gaJS/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._setSampleRate"
-   * >{@literal _setSampleRate}</a> here because that would affect all events in the session.
+   * >{@literal _setSampleRate}</a> here because that would affect all events in
+   * the session.
    */
-  private float random = ForPlay.random();
+  private float random;
 
   public HtmlAnalytics() {
+    random = ForPlay.random();
+    String val = Window.Location.getParameter("analytics");
+    if (val != null) {
+      try {
+        random = Float.parseFloat(val);
+      } catch (Exception ignore) {
+      }
+    }
     log().debug("Analytics random = " + random);
   }
 
   @Override
-  public void logEvent(float sampleRate, String category, String event) {
+  public void logEvent(Category category, String action) {
+    float sampleRate = category.getSampleRate();
     boolean shouldLog = shouldLogEvent(sampleRate);
     ForPlay.log().debug(
-        "Analytics#logEvent(" + sampleRate + ", " + category + ", " + event + ") => "
-            + (shouldLog ? "Logging" : "NOT logging"));
+        "Analytics#logEvent(sampleRate=" + sampleRate + ", cagegory=" + category + ", action="
+            + action + ") => " + (shouldLog ? "Logging" : "NOT logging"));
     if (shouldLog) {
-      logEventImpl(category, event);
+      logEventImpl(category.getCategory(), action);
     }
   }
 
   @Override
-  public void logEvent(float sampleRate, String category, String event, String label, int value) {
+  public void logEvent(Category category, String action, String label, int value) {
+    float sampleRate = category.getSampleRate();
     boolean shouldLog = shouldLogEvent(sampleRate);
     ForPlay.log().debug(
-        "Analytics#logEvent(" + sampleRate + ", " + category + ", " + event + ", " + label + ", "
-            + value + ") => " + (shouldLog ? "Logging" : "NOT logging"));
+        "Analytics#logEvent(sampleRate=" + sampleRate + ", category=" + category + ", action="
+            + action + ", label=" + label + ", value=" + value + ") => "
+            + (shouldLog ? "Logging" : "NOT logging"));
     if (shouldLog) {
-      logEventImpl(category, event, label, value);
+      logEventImpl(category.getCategory(), action, label, value);
     }
   }
 
-  public native void logEventImpl(String category, String event) /*-{
-    $wnd._gaq.push([
-        '_trackEvent', category, event
-    ]);
+  public native void logEventImpl(String category, String action) /*-{
+		$wnd._gaq.push([ '_trackEvent', category, event ]);
   }-*/;
 
-  public native void logEventImpl(String category, String event, String label, int value) /*-{
-    $wnd._gaq.push([
-        '_trackEvent', category, event, label, value
-    ]);
+  public native void logEventImpl(String category, String action, String label, int value) /*-{
+		$wnd._gaq.push([ '_trackEvent', category, action, label, value ]);
   }-*/;
 
   private boolean shouldLogEvent(float sampleRate) {
