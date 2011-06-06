1ba933949259d4bafb38b6529221f32800cb9cf0
Fred Sauer <fredsa@google.com>
2011-06-06 23:58:29 +0000

    Use CrossSiteIframeLinker, which supports dev mode, instead of deprecated XSLinker TODO: Port over DirectInstallLinker, which allows for JS debugging

--- core/src/forplay/appcachelinker/AppCacheLinker.java
+++ core/src/forplay/appcachelinker/AppCacheLinker.java
@@ -26,7 +26,7 @@ import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.linker.Artifact;
 import com.google.gwt.core.ext.linker.ArtifactSet;
 import com.google.gwt.core.ext.linker.EmittedArtifact;
-import com.google.gwt.core.linker.XSLinker;
+import com.google.gwt.core.linker.CrossSiteIframeLinker;
 
 /**
  * AppCacheLinker - linker for public path resources in the Application Cache. <br/>
@@ -54,7 +54,7 @@ import com.google.gwt.core.linker.XSLinker;
  * <p>
  * To add additional static files to the manifest, override {@link #staticCachedFiles()}.
  */
-public class AppCacheLinker extends XSLinker {
+public class AppCacheLinker extends CrossSiteIframeLinker {
 
   private static final HashSet<String> DEFAULT_EXTENSION_WHITELIST = new HashSet<String>(
       Arrays.asList(new String[] {
