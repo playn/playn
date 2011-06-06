fafb54246010064e4d22948048afd1893ca039cd
Fred Sauer <fredsa@google.com>
2011-06-06 16:02:07 +0000

    Fix copyright/license notices

--- core/src/flash/display/Bitmap.java
+++ core/src/flash/display/Bitmap.java
@@ -1,13 +1,23 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 package flash.display;
 
 import flash.gwt.FlashImport;
 
-/**
- * @author cromwellian@google.com (Your Name Here)
- *
- */
 @FlashImport({"flash.display.Bitmap"})
 final public class Bitmap extends DisplayObject {
   protected Bitmap() {}
