cc9c9d50059411d382331698221d02227a420c1d
Philip Rogers <pdr@google.com>
2011-08-16 20:52:04 -0400

    Add Layer.Util class for transforming points between Layers

--- core/src/playn/core/Layer.java
+++ core/src/playn/core/Layer.java
@@ -15,6 +15,8 @@
  */
 package playn.core;
 
+import pythagoras.f.IPoint;
+import pythagoras.f.Point;
 import pythagoras.f.Transform;
 
 /**
@@ -193,5 +195,81 @@ public interface Layer {
      * Return the height of the layer after applying scale.
      */
     public float scaledHeight();
+  }
+
+  /**
+   * TODO(pdr): doc dis
+   */
+  public static class Util {
+    /**
+     * Converts the supplied point from coordinates relative to the specified
+     * layer to screen coordinates. The results are stored into {@code into},
+     * which is returned for convenience.
+     */
+    public static Point layerToScreen(Layer layer, IPoint point, Point into) {
+      return layerToParent(layer, null, point, into);
+    }
+
+    /**
+     * Converts the supplied point from coordinates relative to the specified
+     * layer to screen coordinates. The results are stored into {@code into},
+     * which is returned for convenience.
+     */
+    public static Point layerToScreen(Layer layer, float x, float y, Point into) {
+      return layerToScreen(layer, into.set(x, y), into);
+    }
+
+    /**
+     * Converts the supplied point from coordinates relative to the specified
+     * child layer to coordinates relative to the specified parent layer. The
+     * results are stored into {@code into}, which is returned for convenience.
+     */
+    public static Point layerToParent(Layer layer, Layer parent, IPoint point, Point into) {
+      into.set(point);
+      while (layer != parent) {
+        if (layer == null) {
+          throw new IllegalArgumentException(
+              "Failed to find parent, perhaps you passed parent, layer instead of "
+                  + "layer, parent?");
+        }
+        into.x -= layer.originX();
+        into.y -= layer.originY();
+        layer.transform().transform(into, into);
+        layer = layer.parent();
+      }
+      return into;
+    }
+
+    /**
+     * Converts the supplied point from coordinates relative to the specified
+     * child layer to coordinates relative to the specified parent layer. The
+     * results are stored into {@code into}, which is returned for convenience.
+     */
+    public static Point layerToParent(Layer layer, Layer parent, float x, float y, Point into) {
+      return layerToParent(layer, parent, into.set(x, y), into);
+    }
+
+    /**
+     * Converts the supplied point from screen coordinates to coordinates
+     * relative to the specified layer. The results are stored into {@code into}
+     * , which is returned for convenience.
+     */
+    public static Point screenToLayer(Layer layer, IPoint point, Point into) {
+      Layer parent = layer.parent();
+      IPoint cur = (parent == null) ? point : screenToLayer(parent, point, into);
+      into = layer.transform().inverseTransform(cur, into);
+      into.x += layer.originX();
+      into.y += layer.originY();
+      return into;
+    }
+
+    /**
+     * Converts the supplied point from screen coordinates to coordinates
+     * relative to the specified layer. The results are stored into {@code into}
+     * , which is returned for convenience.
+     */
+    public static Point screenToLayer(Layer layer, float x, float y, Point into) {
+      return screenToLayer(layer, into.set(x, y), into);
+    }
   }
 }
---
+++ core/tests/playn/core/LayerUtilTest.java
@@ -0,0 +1,132 @@
+package playn.core;
+
+import org.junit.Test;
+
+import pythagoras.f.Point;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the Layer.Util class.
+ */
+public class LayerUtilTest {
+  static float tol = 0.001f; // tolerance for floating point equality checks
+
+  @Test public void testTransformWithScale() {
+    TestGroupLayer root = new TestGroupLayer();
+    TestGroupLayer middle = new TestGroupLayer();
+    TestLayer child = new TestLayer();
+    root.add(middle);
+    middle.add(child);
+
+    middle.setScale(2f, 0.1f);
+    Point point = new Point(100f, 100f);
+
+    Point pointOnChild = new Point(0f, 0f);
+    Layer.Util.screenToLayer(child, point, pointOnChild);
+    assertEquals(point.x() * 0.5, pointOnChild.x(), tol);
+    assertEquals(point.y() * 10, pointOnChild.y(), tol);
+
+    Point pointOnParent = new Point(0f, 0f);
+    Layer.Util.layerToScreen(child, pointOnChild, pointOnParent);
+    assertEquals(point.x(), pointOnParent.x(), tol);
+    assertEquals(point.y(), pointOnParent.y(), tol);
+
+    root.clear();
+  }
+
+  @Test public void testTransformWithTrans() {
+    TestGroupLayer root = new TestGroupLayer();
+    TestGroupLayer middle = new TestGroupLayer();
+    TestLayer child = new TestLayer();
+    root.add(middle);
+    middle.add(child);
+
+    middle.setTranslation(10, -10);
+    Point point = new Point(100f, 100f);
+
+    Point pointOnChild = new Point(0f, 0f);
+    Layer.Util.screenToLayer(child, point, pointOnChild);
+    assertEquals(point.x() - 10, pointOnChild.x(), tol);
+    assertEquals(point.y() + 10, pointOnChild.y(), tol);
+
+    Point pointOnParent = new Point(0f, 0f);
+    Layer.Util.layerToScreen(child, pointOnChild, pointOnParent);
+    assertEquals(point.x(), pointOnParent.x(), tol);
+    assertEquals(point.y(), pointOnParent.y(), tol);
+
+    root.clear();
+  }
+
+  @Test public void testTransformWithRot() {
+    TestGroupLayer root = new TestGroupLayer();
+    TestGroupLayer middle = new TestGroupLayer();
+    TestLayer child = new TestLayer();
+    root.add(middle);
+    middle.add(child);
+
+    middle.setRotation((float)(Math.PI / 4.0));
+    Point point = new Point(100f, 100f);
+
+    Point pointOnChild = new Point(0f, 0f);
+    Layer.Util.screenToLayer(child, point, pointOnChild);
+    assertEquals(141.421356, pointOnChild.x(), tol);
+    assertEquals(0, pointOnChild.y(), tol);
+
+    Point pointOnParent = new Point(0f, 0f);
+    Layer.Util.layerToScreen(child, pointOnChild, pointOnParent);
+    assertEquals(point.x(), pointOnParent.x(), tol);
+    assertEquals(point.y(), pointOnParent.y(), tol);
+
+    root.clear();
+  }
+
+  @Test public void testTransformWithScaleRotTrans() {
+    TestGroupLayer root = new TestGroupLayer();
+    TestGroupLayer middle = new TestGroupLayer();
+    TestLayer child = new TestLayer();
+    root.add(middle);
+    middle.add(child);
+
+    middle.setRotation((float)(Math.PI / 4.0));
+    middle.setTranslation(10, -10);
+    middle.setScale(10f, 0.5f);
+    Point point = new Point(100f, 100f);
+
+    Point pointOnChild = new Point(0f, 0f);
+    Layer.Util.screenToLayer(child, point, pointOnChild);
+    Point pointOnParent = new Point(0f, 0f);
+    Layer.Util.layerToScreen(child, pointOnChild, pointOnParent);
+    assertEquals(point.x(), pointOnParent.x(), tol);
+    assertEquals(point.y(), pointOnParent.y(), tol);
+
+    root.clear();
+  }
+
+  protected static class TestLayer extends AbstractLayer {
+  }
+
+  protected static class TestGroupLayer extends AbstractLayer implements GroupLayer {
+    public final GroupLayerImpl<AbstractLayer> impl = new GroupLayerImpl<AbstractLayer>();
+    public Layer get(int index) {
+      return impl.children.get(index);
+    }
+    public void add(Layer layer) {
+      impl.add(this, (AbstractLayer)layer);
+    }
+    @Deprecated public void add(int index, Layer layer) {
+      impl.add(this, index, (AbstractLayer)layer);
+    }
+    public void remove(Layer layer) {
+      impl.remove(this, (AbstractLayer)layer);
+    }
+    @Deprecated public void remove(int index) {
+      impl.remove(this, index);
+    }
+    public void clear() {
+      impl.clear(this);
+    }
+    public int size() {
+      return impl.children.size();
+    }
+  }
+}
