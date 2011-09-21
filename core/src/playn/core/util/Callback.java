720048ef2e42b73935f00e9b1672455c913cc936
Michael Bayne <mdb@samskivert.com>
2011-09-21 15:45:52 -0700

    Added Callback<T> and CallbackList<T> for generic async needs.
    Libraries built for PlayN applications will have need of a platform-neutral
    callback interface. Rather than having each library define its own callback
    interface, we benefit from having PlayN define one generally usable interface.
    
    This will allow client code to easily wait for multiple callbacks to complete
    before proceeding, even if those callbacks come from different libraries.
    
    ResourceCallback should be converted to the (more general) Callback, and I
    would like also to convert AssetWatcher into a more general purpose callback
    aggregation utility.

---
+++ core/src/playn/core/util/Callback.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2011 The PlayN Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package playn.core.util;
+
+/**
+ * Used to communicate asynchronous results.
+ */
+public interface Callback<T>
+{
+  /** A callback that chains failure to the supplied delegate callback. */
+  public static abstract class Chain<T> implements Callback<T> {
+    private Callback<?> onFailure;
+
+    public Chain(Callback<?> onFailure) {
+      this.onFailure = onFailure;
+    }
+
+    @Override
+    public void onFailure(Throwable cause) {
+      onFailure.onFailure(cause);
+    }
+  }
+
+  /**
+   * Called when the asynchronous request succeeded, supplying its result.
+   */
+  void onSuccess(T result);
+
+  /**
+   * Called when the asynchronous request failed, supplying a cause for failure.
+   */
+  void onFailure(Throwable cause);
+}
