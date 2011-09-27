5144c7368d450dbdec76cb6a7eefc1a45af9a9b8
Michael Bayne <mdb@samskivert.com>
2011-09-27 16:08:21 -0700

    Only listen for mouse or touch events, not both.
    Devices that support touch (may) also emit mouse events for the same user
    action. Mobile Safari does this, and moreover the mouse events are synthesized
    to try to make mouse-only apps behave reasonably well, so the mouse events
    don't even accurately reflect the touch behavior.
    
    If a device comes along that supports both mouse and touch input, games which
    wish to target it will have to use Mouse and Touch directly, rather than
    Pointer. It is currently not possible to identify such devices, and attempting
    to unify the two event streams in the way that we previously did results in
    unusably bad default behavior on the most popular touch device in the known
    universe.

--- html/src/playn/html/HtmlPointer.java
+++ html/src/playn/html/HtmlPointer.java
@@ -15,126 +15,134 @@ package playn.html;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.TouchEvent;
 
 import playn.core.PlayN;
 import playn.core.Pointer;
 
 class HtmlPointer extends HtmlInput implements Pointer {
   private Listener listener;
-  boolean inDragSequence = false; // true when we are in a drag sequence (after pointer start but before pointer end)
+  // true when we are in a drag sequence (after pointer start but before pointer end)
+  private boolean inDragSequence = false;
+
+  @Override
+  public void setListener(Listener listener) {
+    this.listener = listener;
+  }
 
   HtmlPointer(final Element rootElement) {
-    // capture mouse down on the root element, only.
-    captureEvent(rootElement, "mousedown", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null) {
-          inDragSequence = true;
-
-          Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
-              rootElement), getRelativeY(nativeEvent, rootElement));
-          listener.onPointerStart(event);
-          if (event.getPreventDefault()) {
-            nativeEvent.preventDefault();
+    // if touch events are supported, we want to use touch handlers; otherwise we use mouse event
+    // handlers; we cannot use both because some browsers (mobile webkit) emit both touch and mouse
+    // events for the same user action
+    if (TouchEvent.isSupported()) {
+      // capture touch start on the root element, only.
+      captureEvent(rootElement, "touchstart", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null) {
+            if (nativeEvent.getChangedTouches().length() > 0) {
+              inDragSequence = true;
+              com.google.gwt.dom.client.Touch touch = nativeEvent.getChangedTouches().get(0);
+              float x = touch.getRelativeX(rootElement);
+              float y = touch.getRelativeY(rootElement);
+              Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+              listener.onPointerStart(event);
+              if (event.getPreventDefault()) {
+                nativeEvent.preventDefault();
+              }
+            }
           }
         }
-      }
-    });
+      });
 
-    // capture mouse up anywhere on the page as long as we are in a drag sequence
-    capturePageEvent("mouseup", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null && inDragSequence) {
-          inDragSequence = false;
-
-          Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
-              rootElement), getRelativeY(nativeEvent, rootElement));
-          listener.onPointerEnd(event);
-          if (event.getPreventDefault()) {
-            nativeEvent.preventDefault();
+      // capture touch end anywhere on the page as long as we are in a drag sequence
+      capturePageEvent("touchend", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null && inDragSequence) {
+            if (nativeEvent.getChangedTouches().length() > 0) {
+              inDragSequence = false;
+              com.google.gwt.dom.client.Touch touch = nativeEvent.getChangedTouches().get(0);
+              float x = touch.getRelativeX(rootElement);
+              float y = touch.getRelativeY(rootElement);
+              Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+              listener.onPointerEnd(event);
+              if (event.getPreventDefault()) {
+                nativeEvent.preventDefault();
+              }
+            }
           }
         }
-      }
-    });
+      });
 
-    // capture mouse move anywhere on the page that fires only if we are in a drag sequence
-    capturePageEvent("mousemove", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null && inDragSequence) {
-          Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
-              rootElement), getRelativeY(nativeEvent, rootElement));
-          listener.onPointerDrag(event);
-          if (event.getPreventDefault()) {
-            nativeEvent.preventDefault();
+      // capture touch move anywhere on the page as long as we are in a drag sequence
+      capturePageEvent("touchmove", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null && inDragSequence) {
+            if (nativeEvent.getChangedTouches().length() > 0) {
+              com.google.gwt.dom.client.Touch touch = nativeEvent.getChangedTouches().get(0);
+              float x = touch.getRelativeX(rootElement);
+              float y = touch.getRelativeY(rootElement);
+              Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+              listener.onPointerDrag(event);
+              if (event.getPreventDefault()) {
+                nativeEvent.preventDefault();
+              }
+            }
           }
         }
-      }
-    });
+      });
 
-    // capture touch start on the root element, only.
-    captureEvent(rootElement, "touchstart", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null) {
-          if (nativeEvent.getTouches().length() > 0) {
+    } else {
+      // capture mouse down on the root element, only.
+      captureEvent(rootElement, "mousedown", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null) {
             inDragSequence = true;
-            com.google.gwt.dom.client.Touch touch = nativeEvent.getTouches().get(0);
-            float x = touch.getRelativeX(rootElement);
-            float y = touch.getRelativeY(rootElement);
-            Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+
+            Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
+                rootElement), getRelativeY(nativeEvent, rootElement));
             listener.onPointerStart(event);
             if (event.getPreventDefault()) {
               nativeEvent.preventDefault();
             }
           }
         }
-      }
-    });
+      });
 
-    // capture touch end anywhere on the page as long as we are in a drag sequence
-    capturePageEvent("touchend", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null && inDragSequence) {
-          if (nativeEvent.getChangedTouches().length() > 0) {
+      // capture mouse up anywhere on the page as long as we are in a drag sequence
+      capturePageEvent("mouseup", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null && inDragSequence) {
             inDragSequence = false;
-            com.google.gwt.dom.client.Touch touch = nativeEvent.getChangedTouches().get(0);
-            float x = touch.getRelativeX(rootElement);
-            float y = touch.getRelativeY(rootElement);
-            Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+
+            Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
+                rootElement), getRelativeY(nativeEvent, rootElement));
             listener.onPointerEnd(event);
             if (event.getPreventDefault()) {
               nativeEvent.preventDefault();
             }
           }
         }
-      }
-    });
+      });
 
-    // capture touch move anywhere on the page as long as we are in a drag sequence
-    capturePageEvent("touchmove", new EventHandler() {
-      @Override
-      public void handleEvent(NativeEvent nativeEvent) {
-        if (listener != null && inDragSequence) {
-          if (nativeEvent.getTouches().length() > 0) {
-            com.google.gwt.dom.client.Touch touch = nativeEvent.getTouches().get(0);
-            float x = touch.getRelativeX(rootElement);
-            float y = touch.getRelativeY(rootElement);
-            Event.Impl event = new Event.Impl(PlayN.currentTime(), x, y);
+      // capture mouse move anywhere on the page that fires only if we are in a drag sequence
+      capturePageEvent("mousemove", new EventHandler() {
+        @Override
+        public void handleEvent(NativeEvent nativeEvent) {
+          if (listener != null && inDragSequence) {
+            Event.Impl event = new Event.Impl(PlayN.currentTime(), getRelativeX(nativeEvent,
+                rootElement), getRelativeY(nativeEvent, rootElement));
             listener.onPointerDrag(event);
             if (event.getPreventDefault()) {
               nativeEvent.preventDefault();
             }
           }
         }
-      }
-    });
-  }
-
-  @Override
-  public void setListener(Listener listener) {
-    this.listener = listener;
+      });
+    }
   }
 }
