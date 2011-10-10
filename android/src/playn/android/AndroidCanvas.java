665e2cd70e75910a286bf0276bf2a266eb35d3b3
Michael Bayne <mdb@samskivert.com>
2011-10-10 11:52:31 -0700

    Implemented fluency in Android Canvas.

--- android/src/playn/android/AndroidCanvas.java
+++ android/src/playn/android/AndroidCanvas.java
@@ -18,24 +18,24 @@ package playn.android;
 import java.util.LinkedList;
 
 import playn.core.Asserts;
+import playn.core.Canvas;
 import playn.core.Gradient;
 import playn.core.Image;
 import playn.core.Path;
 import playn.core.Pattern;
 import playn.core.TextLayout;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
-class AndroidCanvas implements playn.core.Canvas {
+class AndroidCanvas implements Canvas {
   private static Matrix m = new Matrix();
   private static Rect rect = new Rect();
   private static RectF rectf = new RectF();
 
-  private final Canvas canvas;
+  private final android.graphics.Canvas canvas;
   private final Bitmap bitmap;
   private boolean dirty = true;
 
@@ -43,40 +43,44 @@ class AndroidCanvas implements playn.core.Canvas {
 
   AndroidCanvas(int width, int height) {
     bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-    canvas = new Canvas(bitmap);
+    canvas = new android.graphics.Canvas(bitmap);
     paintStack.addFirst(new AndroidCanvasState());
   }
 
   AndroidCanvas(Bitmap bitmap) {
     this.bitmap = bitmap;
-    canvas = new Canvas(bitmap);
+    canvas = new android.graphics.Canvas(bitmap);
     paintStack.addFirst(new AndroidCanvasState());
   }
 
   @Override
-  public void clear() {
+  public Canvas clear() {
     canvas.drawColor(0, PorterDuff.Mode.SRC);
     dirty = true;
+    return this;
   }
 
   @Override
-  public void clip(Path clipPath) {
+  public Canvas clip(Path clipPath) {
     Asserts.checkArgument(clipPath instanceof AndroidPath);
     canvas.clipPath(((AndroidPath) clipPath).path);
+    return this;
   }
 
   @Override
-  public void drawImage(Image img, float x, float y) {
+  public Canvas drawImage(Image img, float x, float y) {
     drawImage(img, x, y, img.width(), img.height());
+    return this;
   }
 
   @Override
-  public void drawImage(Image img, float x, float y, float w, float h) {
+  public Canvas drawImage(Image img, float x, float y, float w, float h) {
     drawImage(img, x, y, w, h, 0, 0, img.width(), img.height());
+    return this;
   }
 
   @Override
-  public void drawImage(Image img, float dx, float dy, float dw, float dh, float sx, float sy,
+  public Canvas drawImage(Image img, float dx, float dy, float dw, float dh, float sx, float sy,
       float sw, float sh) {
     Asserts.checkArgument(img instanceof AndroidImage);
     Bitmap bitmap = ((AndroidImage) img).getBitmap();
@@ -86,59 +90,68 @@ class AndroidCanvas implements playn.core.Canvas {
       canvas.drawBitmap(bitmap, rect, rectf, currentState().prepareImage());
     }
     dirty = true;
+    return this;
   }
 
   @Override
-  public void drawImageCentered(Image image, float dx, float dy) {
+  public Canvas drawImageCentered(Image image, float dx, float dy) {
     drawImage(image, dx - image.width() / 2, dy - image.height() / 2);
     dirty = true;
+    return this;
   }
 
   @Override
-  public void drawLine(float x0, float y0, float x1, float y1) {
+  public Canvas drawLine(float x0, float y0, float x1, float y1) {
     canvas.drawLine(x0, y0, x1, y1, currentState().prepareStroke());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void drawPoint(float x, float y) {
+  public Canvas drawPoint(float x, float y) {
     canvas.drawPoint(x, y, currentState().prepareStroke());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void drawText(String text, float x, float y) {
+  public Canvas drawText(String text, float x, float y) {
     canvas.drawText(text, x, y, currentState().prepareFill());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void drawText(TextLayout layout, float x, float y) {
+  public Canvas drawText(TextLayout layout, float x, float y) {
     ((AndroidTextLayout)layout).draw(canvas, x, y);
     dirty = true;
+    return this;
   }
 
   @Override
-  public void fillCircle(float x, float y, float radius) {
+  public Canvas fillCircle(float x, float y, float radius) {
     canvas.drawCircle(x, y, radius, currentState().prepareFill());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void fillPath(Path path) {
+  public Canvas fillPath(Path path) {
     Asserts.checkArgument(path instanceof AndroidPath);
     canvas.drawPath(((AndroidPath) path).path, currentState().prepareFill());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void fillRect(float x, float y, float width, float height) {
+  public Canvas fillRect(float x, float y, float width, float height) {
     float left = x;
     float top = y;
     float right = left + width;
     float bottom = top + height;
     canvas.drawRect(left, top, right, bottom, currentState().prepareFill());
     dirty = true;
+    return this;
   }
 
   @Override
@@ -147,31 +160,36 @@ class AndroidCanvas implements playn.core.Canvas {
   }
 
   @Override
-  public void restore() {
+  public Canvas restore() {
     canvas.restore();
     paintStack.removeFirst();
 
     Asserts.check(paintStack.size() > 0, "Unbalanced save/restore");
+    return this;
   }
 
   @Override
-  public void rotate(float angle) {
+  public Canvas rotate(float angle) {
     canvas.rotate(rad2deg(angle));
+    return this;
   }
 
   @Override
-  public void save() {
+  public Canvas save() {
     canvas.save();
     paintStack.addFirst(new AndroidCanvasState(currentState()));
+    return this;
   }
 
   @Override
-  public void scale(float x, float y) {
+  public Canvas scale(float x, float y) {
     canvas.scale(x, y);
+    return this;
   }
 
-  public void setAlpha(float alpha) {
+  public Canvas setAlpha(float alpha) {
     currentState().setAlpha(alpha);
+    return this;
   }
 
   public float alpha() {
@@ -179,92 +197,107 @@ class AndroidCanvas implements playn.core.Canvas {
   }
 
   @Override
-  public void setCompositeOperation(Composite composite) {
+  public Canvas setCompositeOperation(Composite composite) {
     currentState().setCompositeOperation(composite);
+    return this;
   }
 
   @Override
-  public void setFillColor(int color) {
+  public Canvas setFillColor(int color) {
     currentState().setFillColor(color);
+    return this;
   }
 
   @Override
-  public void setFillGradient(Gradient gradient) {
+  public Canvas setFillGradient(Gradient gradient) {
     Asserts.checkArgument(gradient instanceof AndroidGradient);
     currentState().setFillGradient((AndroidGradient) gradient);
+    return this;
   }
 
   @Override
-  public void setFillPattern(Pattern pattern) {
+  public Canvas setFillPattern(Pattern pattern) {
     Asserts.checkArgument(pattern instanceof AndroidPattern);
     currentState().setFillPattern((AndroidPattern) pattern);
+    return this;
   }
 
   @Override
-  public void setLineCap(LineCap cap) {
+  public Canvas setLineCap(LineCap cap) {
     currentState().setLineCap(cap);
+    return this;
   }
 
   @Override
-  public void setLineJoin(LineJoin join) {
+  public Canvas setLineJoin(LineJoin join) {
     currentState().setLineJoin(join);
+    return this;
   }
 
   @Override
-  public void setMiterLimit(float miter) {
+  public Canvas setMiterLimit(float miter) {
     currentState().setMiterLimit(miter);
+    return this;
   }
 
   @Override
-  public void setStrokeColor(int color) {
+  public Canvas setStrokeColor(int color) {
     currentState().setStrokeColor(color);
+    return this;
   }
 
   @Override
-  public void setStrokeWidth(float strokeWidth) {
+  public Canvas setStrokeWidth(float strokeWidth) {
     currentState().setStrokeWidth(strokeWidth);
+    return this;
   }
 
   @Override
-  public void setTransform(float m11, float m12, float m21, float m22, float dx, float dy) {
+  public Canvas setTransform(float m11, float m12, float m21, float m22, float dx, float dy) {
     Matrix m = new Matrix();
     // TODO(jgw): Is this the right order?
     m.setValues(new float[] {m11, m12, 0, m21, 0, m22, dx, dy, 1});
     canvas.setMatrix(m);
+    return this;
   }
 
   @Override
-  public void strokeCircle(float x, float y, float radius) {
+  public Canvas strokeCircle(float x, float y, float radius) {
     canvas.drawCircle(x, y, radius, currentState().prepareStroke());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void strokePath(Path path) {
+  public Canvas strokePath(Path path) {
     Asserts.checkArgument(path instanceof AndroidPath);
     canvas.drawPath(((AndroidPath) path).path, currentState().prepareStroke());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void strokeRect(float x, float y, float width, float height) {
+  public Canvas strokeRect(float x, float y, float width, float height) {
     float left = x;
     float top = y;
     float right = left + width;
     float bottom = top + height;
     canvas.drawRect(left, top, right, bottom, currentState().prepareStroke());
     dirty = true;
+    return this;
   }
 
   @Override
-  public void transform(float m11, float m12, float m21, float m22, float dx, float dy) {
+  public Canvas transform(float m11, float m12, float m21, float m22, float dx, float dy) {
     m.setValues(new float[] {m11, m21, dx, m12, m22, dy, 0, 0, 1});
     canvas.concat(m);
+    return this;
   }
 
   @Override
-  public void translate(float x, float y) {
+  public Canvas translate(float x, float y) {
     canvas.translate(x, y);
+    return this;
   }
 
   @Override
