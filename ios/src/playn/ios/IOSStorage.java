5f591b13d24283e56bd1655b039fdc0ed05fda1d
Michael Bayne <mdb@samskivert.com>
2012-01-17 16:20:21 -0800

    Storage implementation, built atop Sqlite.

--- ios/src/playn/ios/IOSStorage.java
+++ ios/src/playn/ios/IOSStorage.java
@@ -15,32 +15,137 @@
  */
 package playn.ios;
 
+import cli.System.Environment.SpecialFolder;
+import cli.System.Environment;
+import cli.System.IO.File;
+import cli.System.IO.Path;
+
+import cli.Mono.Data.Sqlite.SqliteConnection;
+import cli.System.Data.CommandType;
+import cli.System.Data.Common.DbCommand;
+import cli.System.Data.Common.DbDataReader;
+import cli.System.Data.Common.DbParameter;
+import cli.System.Data.DbType;
+import cli.System.Data.IDataReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import playn.core.PlayN;
 import playn.core.Storage;
 
 class IOSStorage implements Storage
 {
+  private static final String STORAGE_FILE_NAME = "playn.db";
+  private static final String STORAGE_SCHEMA =
+    "CREATE TABLE Data (DataKey ntext PRIMARY KEY, DataValue ntext NOT NULL)";
+
+  private SqliteConnection conn;
+
+  IOSStorage() {
+    String docDir = Environment.GetFolderPath(SpecialFolder.wrap(SpecialFolder.Personal));
+    String db = Path.Combine(docDir, STORAGE_FILE_NAME);
+    boolean needCreate = !File.Exists(db);
+    if (needCreate)
+      SqliteConnection.CreateFile(db);
+    conn = new SqliteConnection("Data Source=" + db);
+    if (needCreate)
+      executeUpdate(createCommand(STORAGE_SCHEMA));
+  }
+
   @Override
-  public void setItem(String key, String data) throws RuntimeException {
-    throw new RuntimeException("TODO");
+  public void setItem(String key, String value) throws RuntimeException {
+    // first try to update
+    DbCommand cmd = createCommand("update Data set DataValue = @value where DataKey = @key");
+    cmd.get_Parameters().Add(createParam(cmd, "@value", value));
+    cmd.get_Parameters().Add(createParam(cmd, "@key", key));
+
+    // if that modified zero rows, then insert
+    if (executeUpdate(cmd) == 0) {
+      cmd = createCommand("insert into Data (DataKey, DataValue) values (@key, @value)");
+      cmd.get_Parameters().Add(createParam(cmd, "@key", key));
+      cmd.get_Parameters().Add(createParam(cmd, "@value", value));
+      if (executeUpdate(cmd) == 0) {
+        PlayN.log().warn("Failed to insert storage item [key=" + key + "]");
+      }
+    }
   }
 
   @Override
   public void removeItem(String key) {
-    throw new RuntimeException("TODO");
+    DbCommand cmd = createCommand("delete from Data where DataKey = @key");
+    cmd.get_Parameters().Add(createParam(cmd, "@key", key));
+    executeUpdate(cmd);
   }
 
   @Override
   public String getItem(String key) {
-    throw new RuntimeException("TODO");
+    DbCommand cmd = createCommand("select DataValue from Data where DataKey = @key");
+    cmd.get_Parameters().Add(createParam(cmd, "@key", key));
+
+    IDataReader reader = null;
+    try {
+      conn.Open();
+      reader = cmd.ExecuteReader();
+      return reader.Read() ? reader.GetString(0) : null;
+
+    } finally {
+      conn.Close();
+      if (reader != null)
+        reader.Dispose();
+      cmd.Dispose();
+    }
   }
 
   @Override
   public Iterable<String> keys() {
-    throw new RuntimeException("TODO");
+    List<String> keys = new ArrayList<String>();
+    DbCommand cmd = createCommand("select DataKey from Data");
+
+    IDataReader reader = null;
+    try {
+      conn.Open();
+      reader = cmd.ExecuteReader();
+      while (reader.Read()) {
+        keys.add(reader.GetString(0));
+      }
+      return keys;
+
+    } finally {
+      conn.Close();
+      if (reader != null)
+        reader.Dispose();
+      cmd.Dispose();
+    }
   }
 
   @Override
   public boolean isPersisted() {
-    throw new RuntimeException("TODO");
+    return true;
+  }
+
+  private DbParameter createParam(DbCommand cmd, String name, String value) {
+    DbParameter param = cmd.CreateParameter();
+    param.set_ParameterName(name);
+    param.set_DbType(DbType.wrap(DbType.String));
+    param.set_Value(value);
+    return param;
+  }
+
+  private DbCommand createCommand(String sql) {
+    DbCommand cmd = conn.CreateCommand();
+    cmd.set_CommandText(sql);
+    cmd.set_CommandType(CommandType.wrap(CommandType.Text));
+    return cmd;
+  }
+
+  private int executeUpdate(DbCommand cmd) {
+    try {
+      conn.Open();
+      return cmd.ExecuteNonQuery();
+    } finally {
+      conn.Close();
+      cmd.Dispose();
+    }
   }
 }
