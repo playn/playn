afff8896f546fd35da2403d5ec8fc084d122fcc0
Michael Bayne <mdb@samskivert.com>
2012-02-09 13:34:46 -0800

    Added iOS module to test project.

---
+++ tests/ios/Main.cs
@@ -0,0 +1,29 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using playn.ios;
+using playn.core;
+using playn.tests.core;
+
+namespace playn.tests.ios
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			IOSPlatform.register(app);
+		    PlayN.run(new TestsGame());
+    		return true;
+		}
+	}
+
+	public class Application
+	{
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
---
+++ tests/ios/rebuild-dll.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$IKVM_HOME" ]; then
+    IKVM_HOME=$HOME/projects/ikvm-monotouch
+fi
+MAVEN_REPO=$HOME/.m2/repository
+export MONO_PATH=/Developer/MonoTouch/usr/lib/mono/2.1
+
+echo "Converting Java bytecode to CLR dll..."
+rm -f tests-core.dll
+mono $IKVM_HOME/bin/ikvmc.exe -nostdlib -debug -target:library -out:tests-core.dll \
+    -r:$MONO_PATH/mscorlib.dll \
+    -r:$MONO_PATH/System.dll \
+    -r:$MONO_PATH/System.Core.dll \
+    -r:$MONO_PATH/System.Data.dll \
+    -r:$MONO_PATH/OpenTK.dll \
+    -r:$MONO_PATH/monotouch.dll \
+    -r:$MONO_PATH/Mono.Data.Sqlite.dll \
+    $MAVEN_REPO/com/samskivert/pythagoras/1.1/pythagoras-1.1.jar \
+    $MAVEN_REPO/com/googlecode/playn/playn-core/1.1-SNAPSHOT/playn-core-1.1-SNAPSHOT.jar \
+    $MAVEN_REPO/com/googlecode/playn/playn-ios/1.1-SNAPSHOT/playn-ios-1.1-SNAPSHOT.jar \
+    ../core/target/playn-tests-core-1.1-SNAPSHOT.jar
