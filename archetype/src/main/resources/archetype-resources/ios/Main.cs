8bedb02f32b5e1e486f932a8fa219d4ea7ac201b
Michael Bayne <mdb@samskivert.com>
2012-04-09 15:48:09 -0700

    Added iOS submodule to archetype. Warning, this may change!

---
+++ archetype/src/main/resources/archetype-resources/ios/Main.cs
@@ -0,0 +1,26 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using playn.ios;
+using playn.core;
+using playn.tests.core;
+
+namespace playn.tests.ios
+{
+  [Register ("AppDelegate")]
+  public partial class AppDelegate : UIApplicationDelegate {
+    public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
+      app.SetStatusBarHidden(true, true);
+      IOSPlatform.register(app, IOSPlatform.SupportedOrients.ALL);
+      PlayN.run(new TestsGame());
+      return true;
+    }
+  }
+
+  public class Application {
+    static void Main (string[] args) {
+      UIApplication.Main (args, null, "AppDelegate");
+    }
+  }
+}
