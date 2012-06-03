82557423237d2234871db948ae37edfbf6849345
Stefan Haustein <stefan.haustein@gmail.com>
2012-06-03 14:45:04 +0200

    nio emulation documentation improvement

--- html/src/playn/html/HasArrayBufferView.java
+++ html/src/playn/html/HasArrayBufferView.java
@@ -17,8 +17,27 @@ package playn.html;
 
 import com.google.gwt.typedarrays.client.ArrayBufferView;
 
+/** 
+ * This class allows us to get direct access to the typed array used by the 
+ * nio buffer emulation in GWT. All nio buffer types implement this
+ * interfaces in GWT mode. This can be used only in HTML-platform 
+ * specific code.
+ */
 public interface HasArrayBufferView {
+    /**
+     * Returns the underlying typed array buffer view.
+     */
     public ArrayBufferView getTypedArray ();
+
+    /**
+     * Returns the element size in bytes (e.g. 4 for a FloatBuffer and
+     * 1 for a ByteBuffer).
+     */
     int getElementSize ();
+    
+    /** 
+     * Returns the open GL element type constant corresponding to the buffer
+     * contents.
+     */
     int getElementType();
 }
