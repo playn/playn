407dff8771b0a7c64412e72a23d86eb42bdf7204
Michael Bayne <mdb@samskivert.com>
2012-06-05 10:54:42 -0700

    Test non-zero origins on clipped group layers.

--- tests/core/src/main/java/playn/tests/core/ClippedGroupTest.java
+++ tests/core/src/main/java/playn/tests/core/ClippedGroupTest.java
@@ -55,8 +55,10 @@ public class ClippedGroupTest extends Test {
       public void render(Surface surf) {
         // draw the border of our various clipped groups
         surf.setFillColor(0xFF000000);
-        drawRect(surf, g1.transform().tx(), g1.transform().ty(), g1.width(), g1.height());
-        drawRect(surf, g2.transform().tx(), g2.transform().ty(), g2.width(), g2.height());
+        drawRect(surf, g1.transform().tx() - g1.originX(), g1.transform().ty() - g1.originY(),
+                 g1.width(), g1.height());
+        drawRect(surf, g2.transform().tx() - g2.originX(), g2.transform().ty() - g2.originY(),
+                 g2.width(), g2.height());
       }
       protected void drawRect(Surface surf, float x, float y, float w, float h) {
         float left = x-1, top = y-1, right = x+w+2, bot = y+h+2;
@@ -69,14 +71,17 @@ public class ClippedGroupTest extends Test {
 
     // create a group layer with a static clip, and a rotating image inside
     g1 = graphics().createGroupLayer(100, 100);
+    // test the origin not being at zero/zero
+    g1.setOrigin(50, 0);
     i1 = graphics().createImageLayer(img);
     i1.setOrigin(i1.width()/2, i1.height()/2);
     g1.addAt(i1, 50, 50);
-    rootLayer.addAt(g1, 25, 25);
+    rootLayer.addAt(g1, 75, 25);
 
     g2 = graphics().createGroupLayer(100, 100);
+    g2.setOrigin(50, 50);
     g2.addAt(graphics().createImageLayer(img), (100 - img.width())/2, (100 - img.height())/2);
-    rootLayer.addAt(g2, 150, 25);
+    rootLayer.addAt(g2, 200, 75);
   }
 
   @Override
