193e733ff866be6b2011d3ebde906e0111516518
Jamie Doornbos <jamie@threerings.net>
2012-07-11 10:47:29 -0700

    Fix bug with pointer dispatch
    Cancellation does occur sometimes, so we need to react to it. Otherwise the pointer becomes
    effectively useless after a Keyboard popup.

--- ios/src/playn/ios/IOSPointer.java
+++ ios/src/playn/ios/IOSPointer.java
@@ -56,7 +56,10 @@ public class IOSPointer extends PointerImpl {
   }
 
   void onTouchesCancelled(NSSet touches, UIEvent event) {
-    // TODO: ???
+      Event.Impl ev = toPointerEvent(touches, event);
+      if (ev != null) {
+        _active = null;
+      }
   }
 
   private Event.Impl toPointerEvent(NSSet touches, UIEvent event) {
