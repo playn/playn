1c5c4694cd55e66e30a3be4188644c7ed7852892
Michael Bayne <mdb@samskivert.com>
2013-02-06 15:24:32 -0800

    Adding tests-html to SBT build, wiring up superdevmode.

--- project/Build.scala
+++ project/Build.scala
@@ -1,26 +1,27 @@
 import sbt._
 import Keys._
 import samskivert.ProjectBuilder
+import net.thunderklaus.GwtPlugin._
 
 object PlayNBuild extends Build {
   val builder = new ProjectBuilder("pom.xml") {
     override val globalSettings = Seq(
       crossPaths   := false,
-      scalaVersion := "2.9.1",
+      scalaVersion := "2.9.2",
       javacOptions ++= Seq("-Xlint", "-Xlint:-serial", "-source", "1.6", "-target", "1.6"),
       javaOptions ++= Seq("-ea"),
       fork in Compile := true,
       autoScalaLibrary := false, // no scala-library dependency
       publishArtifact in (Compile, packageDoc) := false, // no scaladocs; it fails
       resolvers    += "Forplay Legacy" at "http://forplay.googlecode.com/svn/mavenrepo",
-      // everybody gets their source and tests directories rewritten
-      javaSource in Compile <<= baseDirectory / "src",
-      javaSource in Test <<= baseDirectory / "tests",
       // no parallel test execution to avoid confusions
       parallelExecution in Test := false
     )
-    override def projectSettings (name :String) = name match {
+    override def projectSettings (name :String, pom :pomutil.POM) = name match {
       case "core" => Seq(
+        // our source and tests are in non-standard places
+        javaSource in Compile <<= baseDirectory / "src",
+        javaSource in Test <<= baseDirectory / "tests",
         // adds source files to our jar file (needed by GWT)
         unmanagedResourceDirectories in Compile <+= baseDirectory / "src",
         unmanagedBase <<= baseDirectory { base => base / "disabled" },
@@ -30,31 +31,46 @@ object PlayNBuild extends Build {
           "com.novocode" % "junit-interface" % "0.7" % "test->default"
         )
       )
-      case "java" | "android" => Seq(
+      case "java" | "android" => seq(
+        // our source and tests are in non-standard places
+        javaSource in Compile <<= baseDirectory / "src",
+        javaSource in Test <<= baseDirectory / "tests",
         libraryDependencies ++= Seq(
  	        "com.novocode" % "junit-interface" % "0.7" % "test->default"
         )
       )
       case "gwtbox2d" => Seq(
+        // our source and tests are in non-standard places
+        javaSource in Compile <<= baseDirectory / "src",
+        javaSource in Test <<= baseDirectory / "tests",
         // exclude GWT generator code
         excludeFilter in unmanagedSources ~= { _ || "PoolingStackGenerator.java" },
         // adds source files to our jar file (needed by GWT)
         unmanagedResourceDirectories in Compile <+= baseDirectory / "src"
       )
       case "webgl" => Seq(
+        // our source and tests are in non-standard places
+        javaSource in Compile <<= baseDirectory / "src",
+        javaSource in Test <<= baseDirectory / "tests",
         // adds source files to our jar file (needed by GWT)
         unmanagedResourceDirectories in Compile <+= baseDirectory / "src"
       )
       case "html" => Seq(
+        // our source and tests are in non-standard places
+        javaSource in Compile <<= baseDirectory / "src",
+        javaSource in Test <<= baseDirectory / "tests",
         // adds source files to our jar file (needed by GWT)
         unmanagedResourceDirectories in Compile <+= baseDirectory / "src"
       )
-      case "tests-core" => Seq(
-        // copy resources from playn/tests/resources
-        unmanagedResourceDirectories in Compile <+= baseDirectory / "src/main/java",
-        excludeFilter in unmanagedResources ~= { _ || "*.java" }
-      )
       case "tests-java" => LWJGLPlugin.lwjglSettings
+      case "tests-html" => gwtSettings ++ seq(
+        gwtVersion := pom.getAttr("gwt.version").get,
+        javaOptions in Gwt ++= Seq("-mx512M"), // give GWT mo' memory
+        libraryDependencies ++= Seq(
+          "org.mortbay.jetty" % "jetty" % "6.1.22" % "container",
+          "com.novocode" % "junit-interface" % "0.7" % "test->default"
+        )
+      )
       case _ => Nil
     }
   }
@@ -67,11 +83,15 @@ object PlayNBuild extends Build {
   lazy val flash = builder("flash")
   lazy val android = builder("android")
   lazy val ios = builder("ios")
+
   lazy val testsAssets = builder("tests-assets")
   lazy val testsCore = builder("tests-core")
   lazy val testsJava = builder("tests-java")
+  lazy val testsHtml = builder("tests-html")
+  lazy val tests = Project("tests", file("tests")) aggregate(
+    testsAssets, testsCore, testsJava, testsHtml)
 
   // one giant fruit roll-up to bring them all together
   lazy val playn = Project("playn", file(".")) aggregate(
-    core, gwtbox2d, webgl, java, html, flash, android, ios, testsAssets, testsCore, testsJava)
+    core, gwtbox2d, webgl, java, html, flash, android, ios, tests)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,12 @@
-libraryDependencies += "com.samskivert" % "sbt-pom-util" % "0.2"
+// pom-util POM helpers
+libraryDependencies += "com.samskivert" % "sbt-pom-util" % "0.4-SNAPSHOT"
 
-addSbtPlugin("com.github.philcali" % "sbt-lwjgl-plugin" % "3.1.3")
+// a plugin for wiring up LWJGL native libraries
+addSbtPlugin("com.github.philcali" % "sbt-lwjgl-plugin" % "3.1.4")
+
+// TEMP: force use of locally installed version as mine has gwt-superdevmode
+// a Maven repo needed for the sbt-gwt-plugin
+// resolvers += "thunderklaus repo" at "http://thunderklaus.github.com/maven"
+
+// handles GWT stuff for test-html build (also pulls in xsbt-web-plugin)
+addSbtPlugin("net.thunderklaus" % "sbt-gwt-plugin" % "1.1-SNAPSHOT")
