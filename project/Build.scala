4c95e198b51685733cb681d8cd3d69763320cb92
Michael Bayne <mdb@samskivert.com>
2013-06-03 17:35:53 -0700

    Update to new style sbt-pom-util build.

--- project/Build.scala
+++ project/Build.scala
@@ -1,10 +1,10 @@
 import java.io.File
 import sbt._
 import Keys._
-import samskivert.ProjectBuilder
 import net.thunderklaus.GwtPlugin._
 
-object PlayNBuild extends Build {
+object PlayNBuild extends samskivert.MavenBuild {
+
   // our source and tests are in non-standard places
   val srcDirSettings = seq(
     javaSource in Compile <<= baseDirectory / "src",
@@ -20,78 +20,61 @@ object PlayNBuild extends Build {
     def accept (f :File) :Boolean = f.getAbsolutePath.contains(path)
   }
 
-  val builder = new ProjectBuilder("pom.xml") {
-    override val globalSettings = Seq(
-      crossPaths   := false,
-      scalaVersion := "2.9.2",
-      javacOptions ++= Seq("-Xlint", "-Xlint:-serial", "-source", "1.6", "-target", "1.6"),
-      javaOptions ++= Seq("-ea"),
-      fork in Compile := true,
-      autoScalaLibrary := false, // no scala-library dependency
-      publishArtifact in (Compile, packageDoc) := false, // no scaladocs; it fails
-      resolvers    += "Forplay Legacy" at "http://forplay.googlecode.com/svn/mavenrepo",
-      // no parallel test execution to avoid confusions
-      parallelExecution in Test := false
+  override val globalSettings = Seq(
+    crossPaths   := false,
+    scalaVersion := "2.9.2",
+    javacOptions ++= Seq("-Xlint", "-Xlint:-serial", "-source", "1.6", "-target", "1.6"),
+    javaOptions ++= Seq("-ea"),
+    fork in Compile := true,
+    autoScalaLibrary := false, // no scala-library dependency
+    publishArtifact in (Compile, packageDoc) := false, // no scaladocs; it fails
+    resolvers    += "Forplay Legacy" at "http://forplay.googlecode.com/svn/mavenrepo",
+    // no parallel test execution to avoid confusions
+    parallelExecution in Test := false
+  )
+
+  override def moduleSettings (name :String, pom :pomutil.POM) = name match {
+    case "core" => srcDirSettings ++ seq(
+      unmanagedBase <<= baseDirectory { base => base / "disabled" },
+      // tests depends on resource files mixed into source directory, yay!
+      unmanagedResourceDirectories in Test <+= baseDirectory / "tests",
+      libraryDependencies ++= Seq(
+        "com.novocode" % "junit-interface" % "0.7" % "test->default"
+      )
     )
-    override def projectSettings (name :String, pom :pomutil.POM) = name match {
-      case "core" => srcDirSettings ++ seq(
-        unmanagedBase <<= baseDirectory { base => base / "disabled" },
-        // tests depends on resource files mixed into source directory, yay!
-        unmanagedResourceDirectories in Test <+= baseDirectory / "tests",
-        libraryDependencies ++= Seq(
-          "com.novocode" % "junit-interface" % "0.7" % "test->default"
-        )
+    case "java" | "android" => srcDirSettings ++ seq(
+      libraryDependencies ++= Seq(
+        "com.novocode" % "junit-interface" % "0.7" % "test->default"
       )
-      case "java" | "android" => srcDirSettings ++ seq(
-        libraryDependencies ++= Seq(
- 	        "com.novocode" % "junit-interface" % "0.7" % "test->default"
-        )
+    )
+    case "jbox2d" => srcDirSettings ++ seq(
+      // exclude GWT generator and supersource code
+      excludeFilter in unmanagedSources ~= {
+        _ || "PoolingStackGenerator.java" || excludePath("org/jbox2d/emul")
+      }
+    )
+    case "webgl" | "flash" | "ios" => srcDirSettings
+    case "html" => srcDirSettings ++ seq(
+      // exclude GWT supersource code
+      excludeFilter in unmanagedSources ~= { _ || excludePath("playn/super") }
+    )
+    case "tests-assets" => testSettings
+    case "tests-core" => testSettings
+    case "tests-java" => LWJGLPlugin.lwjglSettings ++ testSettings ++ seq(
+      LWJGLPlugin.lwjgl.version := pom.getAttr("lwjgl.version").get
+    )
+    case "tests-html" => gwtSettings ++ testSettings ++ seq(
+      gwtVersion := pom.getAttr("gwt.version").get,
+      javaOptions in Gwt ++= Seq("-mx512M"), // give GWT mo' memory
+      libraryDependencies ++= Seq(
+        "org.mortbay.jetty" % "jetty" % "6.1.22" % "container",
+        "com.novocode" % "junit-interface" % "0.7" % "test->default"
       )
-      case "jbox2d" => srcDirSettings ++ seq(
-        // exclude GWT generator and supersource code
-        excludeFilter in unmanagedSources ~= {
-          _ || "PoolingStackGenerator.java" || excludePath("org/jbox2d/emul")
-        }
-      )
-      case "webgl" | "flash" | "ios" => srcDirSettings
-      case "html" => srcDirSettings ++ seq(
-        // exclude GWT supersource code
-        excludeFilter in unmanagedSources ~= { _ || excludePath("playn/super") }
-      )
-      case "tests-assets" => testSettings
-      case "tests-core" => testSettings
-      case "tests-java" => LWJGLPlugin.lwjglSettings ++ testSettings ++ seq(
-        LWJGLPlugin.lwjgl.version := pom.getAttr("lwjgl.version").get
-      )
-      case "tests-html" => gwtSettings ++ testSettings ++ seq(
-        gwtVersion := pom.getAttr("gwt.version").get,
-        javaOptions in Gwt ++= Seq("-mx512M"), // give GWT mo' memory
-        libraryDependencies ++= Seq(
-          "org.mortbay.jetty" % "jetty" % "6.1.22" % "container",
-          "com.novocode" % "junit-interface" % "0.7" % "test->default"
-        )
-      )
-      case _ => Nil
-    }
+    )
+    case _ => Nil
   }
 
-  lazy val core = builder("core")
-  lazy val jbox2d = builder("jbox2d")
-  lazy val webgl = builder("webgl")
-  lazy val java = builder("java")
-  lazy val html = builder("html")
-  lazy val flash = builder("flash")
-  lazy val android = builder("android")
-  lazy val ios = builder("ios")
-
-  lazy val testsAssets = builder("tests-assets")
-  lazy val testsCore = builder("tests-core")
-  lazy val testsJava = builder("tests-java")
-  lazy val testsHtml = builder("tests-html")
-  lazy val tests = Project("tests", file("tests")) aggregate(
-    testsAssets, testsCore, testsJava, testsHtml)
-
-  // one giant fruit roll-up to bring them all together
-  lazy val playn = Project("playn", file(".")) aggregate(
-    core, jbox2d, webgl, java, html, flash, android, ios, tests)
+  override protected def projects (builder :samskivert.ProjectBuilder) =
+    super.projects(builder) ++ Seq(builder("tests-assets"), builder("tests-core"),
+                                   builder("tests-java"), builder("tests-html"))
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 // pom-util POM helpers
-libraryDependencies += "com.samskivert" % "sbt-pom-util" % "0.4"
+libraryDependencies += "com.samskivert" % "sbt-pom-util" % "0.6-SNAPSHOT"
 
 // a plugin for wiring up LWJGL native libraries
 addSbtPlugin("com.github.philcali" % "sbt-lwjgl-plugin" % "3.1.4")
