ceae604d40e162d62de5856319e5823f8d4ab00a
David Hoover <karma@deadmoose.com>
2013-10-23 14:12:44 -0700

    Oh god, so much keyboard fun.
    Swap over to using the lwjgl constants for sanity's sake, plus map
    a bunch that were missed before.

--- java/src/playn/java/JavaKeyboard.java
+++ java/src/playn/java/JavaKeyboard.java
@@ -83,110 +83,139 @@ class JavaKeyboard implements playn.core.Keyboard {
   }
 
   private Key translateKey(int keyCode) {
-    // TODO(jgw): Confirm that these mappings are correct.
     switch (keyCode) {
-      case 0x01 : return Key.ESCAPE          ;
-      case 0x02 : return Key.K1              ;
-      case 0x03 : return Key.K2              ;
-      case 0x04 : return Key.K3              ;
-      case 0x05 : return Key.K4              ;
-      case 0x06 : return Key.K5              ;
-      case 0x07 : return Key.K6              ;
-      case 0x08 : return Key.K7              ;
-      case 0x09 : return Key.K8              ;
-      case 0x0A : return Key.K9              ;
-      case 0x0B : return Key.K0              ;
-      case 0x0C : return Key.MINUS           ;
-      case 0x0D : return Key.EQUALS          ;
-      case 0x0E : return Key.BACKSPACE       ;
-      case 0x0F : return Key.TAB             ;
-      case 0x10 : return Key.Q               ;
-      case 0x11 : return Key.W               ;
-      case 0x12 : return Key.E               ;
-      case 0x13 : return Key.R               ;
-      case 0x14 : return Key.T               ;
-      case 0x15 : return Key.Y               ;
-      case 0x16 : return Key.U               ;
-      case 0x17 : return Key.I               ;
-      case 0x18 : return Key.O               ;
-      case 0x19 : return Key.P               ;
-      case 0x1A : return Key.LEFT_BRACKET    ;
-      case 0x1B : return Key.RIGHT_BRACKET   ;
-      case 0x1C : return Key.ENTER           ;
-      case 0x1D : return Key.CONTROL         ;
-      case 0x1E : return Key.A               ;
-      case 0x1F : return Key.S               ;
-      case 0x20 : return Key.D               ;
-      case 0x21 : return Key.F               ;
-      case 0x22 : return Key.G               ;
-      case 0x23 : return Key.H               ;
-      case 0x24 : return Key.J               ;
-      case 0x25 : return Key.K               ;
-      case 0x26 : return Key.L               ;
-      case 0x27 : return Key.SEMICOLON       ;
-      case 0x28 : return Key.QUOTE           ;
-      case 0x2A : return Key.SHIFT           ;
-      case 0x2B : return Key.BACKSLASH       ;
-      case 0x2C : return Key.Z               ;
-      case 0x2D : return Key.X               ;
-      case 0x2E : return Key.C               ;
-      case 0x2F : return Key.V               ;
-      case 0x30 : return Key.B               ;
-      case 0x31 : return Key.N               ;
-      case 0x32 : return Key.M               ;
-      case 0x33 : return Key.COMMA           ;
-      case 0x34 : return Key.PERIOD          ;
-      case 0x35 : return Key.SLASH           ;
-      case 0x36 : return Key.SHIFT           ;
-      case 0x37 : return Key.MULTIPLY        ;
-      case 0x38 : return Key.MENU            ;
-      case 0x39 : return Key.SPACE           ;
-      case 0x3A : return Key.CAPS_LOCK       ;
-      case 0x3B : return Key.F1              ;
-      case 0x3C : return Key.F2              ;
-      case 0x3D : return Key.F3              ;
-      case 0x3E : return Key.F4              ;
-      case 0x3F : return Key.F5              ;
-      case 0x40 : return Key.F6              ;
-      case 0x41 : return Key.F7              ;
-      case 0x42 : return Key.F8              ;
-      case 0x43 : return Key.F9              ;
-      case 0x44 : return Key.F10             ;
-      case 0x45 : return Key.NP_NUM_LOCK     ;
-      case 0x46 : return Key.SCROLL_LOCK     ;
-      case 0x47 : return Key.NP7             ;
-      case 0x48 : return Key.NP8             ;
-      case 0x49 : return Key.NP9             ;
-      case 0x4A : return Key.NP_SUBTRACT     ;
-      case 0x4B : return Key.NP4             ;
-      case 0x4C : return Key.NP5             ;
-      case 0x4D : return Key.NP6             ;
-      case 0x4E : return Key.NP_ADD          ;
-      case 0x4F : return Key.NP1             ;
-      case 0x50 : return Key.NP2             ;
-      case 0x51 : return Key.NP3             ;
-      case 0x52 : return Key.NP0             ;
-      case 0x53 : return Key.NP_DECIMAL      ;
-      case 0x57 : return Key.F11             ;
-      case 0x58 : return Key.F12             ;
-      case 0x90 : return Key.CIRCUMFLEX      ;
-      case 0x91 : return Key.AT              ;
-      case 0x92 : return Key.COLON           ;
-      case 0x93 : return Key.UNDERSCORE      ;
-      case 0xB7 : return Key.SYSRQ           ;
-      case 0xC5 : return Key.PAUSE           ;
-      case 0xC7 : return Key.HOME            ;
-      case 0xC8 : return Key.UP              ;
-      case 0xC9 : return Key.PAGE_UP         ;
-      case 0xCB : return Key.LEFT            ;
-      case 0xCD : return Key.RIGHT           ;
-      case 0xCF : return Key.END             ;
-      case 0xD0 : return Key.DOWN            ;
-      case 0xD1 : return Key.PAGE_DOWN       ;
-      case 0xD2 : return Key.INSERT          ;
-      case 0xD3 : return Key.DELETE          ;
-      case 0xDB : return Key.META            ;
-      case 0xDE : return Key.POWER           ;
+      case Keyboard.KEY_ESCAPE       : return Key.ESCAPE;
+      case Keyboard.KEY_1            : return Key.K1;
+      case Keyboard.KEY_2            : return Key.K2;
+      case Keyboard.KEY_3            : return Key.K3;
+      case Keyboard.KEY_4            : return Key.K4;
+      case Keyboard.KEY_5            : return Key.K5;
+      case Keyboard.KEY_6            : return Key.K6;
+      case Keyboard.KEY_7            : return Key.K7;
+      case Keyboard.KEY_8            : return Key.K8;
+      case Keyboard.KEY_9            : return Key.K9;
+      case Keyboard.KEY_0            : return Key.K0;
+      case Keyboard.KEY_MINUS        : return Key.MINUS;
+      case Keyboard.KEY_EQUALS       : return Key.EQUALS;
+      case Keyboard.KEY_BACK         : return Key.BACK;
+      case Keyboard.KEY_TAB          : return Key.TAB;
+      case Keyboard.KEY_Q            : return Key.Q;
+      case Keyboard.KEY_W            : return Key.W;
+      case Keyboard.KEY_E            : return Key.E;
+      case Keyboard.KEY_R            : return Key.R;
+      case Keyboard.KEY_T            : return Key.T;
+      case Keyboard.KEY_Y            : return Key.Y;
+      case Keyboard.KEY_U            : return Key.U;
+      case Keyboard.KEY_I            : return Key.I;
+      case Keyboard.KEY_O            : return Key.O;
+      case Keyboard.KEY_P            : return Key.P;
+      case Keyboard.KEY_LBRACKET     : return Key.LEFT_BRACKET;
+      case Keyboard.KEY_RBRACKET     : return Key.RIGHT_BRACKET;
+      case Keyboard.KEY_RETURN       : return Key.ENTER;
+      case Keyboard.KEY_LCONTROL     : return Key.CONTROL;
+      case Keyboard.KEY_A            : return Key.A;
+      case Keyboard.KEY_S            : return Key.S;
+      case Keyboard.KEY_D            : return Key.D;
+      case Keyboard.KEY_F            : return Key.F;
+      case Keyboard.KEY_G            : return Key.G;
+      case Keyboard.KEY_H            : return Key.H;
+      case Keyboard.KEY_J            : return Key.J;
+      case Keyboard.KEY_K            : return Key.K;
+      case Keyboard.KEY_L            : return Key.L;
+      case Keyboard.KEY_SEMICOLON    : return Key.SEMICOLON;
+      case Keyboard.KEY_APOSTROPHE   : return Key.QUOTE;
+      case Keyboard.KEY_GRAVE        : return Key.BACKQUOTE;
+      case Keyboard.KEY_LSHIFT       : return Key.SHIFT; // PlayN doesn't know left v. right
+      case Keyboard.KEY_BACKSLASH    : return Key.BACKSLASH;
+      case Keyboard.KEY_Z            : return Key.Z;
+      case Keyboard.KEY_X            : return Key.X;
+      case Keyboard.KEY_C            : return Key.C;
+      case Keyboard.KEY_V            : return Key.V;
+      case Keyboard.KEY_B            : return Key.B;
+      case Keyboard.KEY_N            : return Key.N;
+      case Keyboard.KEY_M            : return Key.M;
+      case Keyboard.KEY_COMMA        : return Key.COMMA;
+      case Keyboard.KEY_PERIOD       : return Key.PERIOD;
+      case Keyboard.KEY_SLASH        : return Key.SLASH;
+      case Keyboard.KEY_RSHIFT       : return Key.SHIFT; // PlayN doesn't know left v. right
+      case Keyboard.KEY_MULTIPLY     : return Key.MULTIPLY;
+      case Keyboard.KEY_LMENU        : return Key.ALT; // PlayN doesn't know left v. right
+      case Keyboard.KEY_SPACE        : return Key.SPACE;
+      case Keyboard.KEY_CAPITAL      : return Key.CAPS_LOCK;
+      case Keyboard.KEY_F1           : return Key.F1;
+      case Keyboard.KEY_F2           : return Key.F2;
+      case Keyboard.KEY_F3           : return Key.F3;
+      case Keyboard.KEY_F4           : return Key.F4;
+      case Keyboard.KEY_F5           : return Key.F5;
+      case Keyboard.KEY_F6           : return Key.F6;
+      case Keyboard.KEY_F7           : return Key.F7;
+      case Keyboard.KEY_F8           : return Key.F8;
+      case Keyboard.KEY_F9           : return Key.F9;
+      case Keyboard.KEY_F10          : return Key.F10;
+      case Keyboard.KEY_NUMLOCK      : return Key.NP_NUM_LOCK;
+      case Keyboard.KEY_SCROLL       : return Key.SCROLL_LOCK;
+      case Keyboard.KEY_NUMPAD7      : return Key.NP7;
+      case Keyboard.KEY_NUMPAD8      : return Key.NP8;
+      case Keyboard.KEY_NUMPAD9      : return Key.NP9;
+      case Keyboard.KEY_SUBTRACT     : return Key.NP_SUBTRACT;
+      case Keyboard.KEY_NUMPAD4      : return Key.NP4;
+      case Keyboard.KEY_NUMPAD5      : return Key.NP5;
+      case Keyboard.KEY_NUMPAD6      : return Key.NP6;
+      case Keyboard.KEY_ADD          : return Key.NP_ADD;
+      case Keyboard.KEY_NUMPAD1      : return Key.NP1;
+      case Keyboard.KEY_NUMPAD2      : return Key.NP2;
+      case Keyboard.KEY_NUMPAD3      : return Key.NP3;
+      case Keyboard.KEY_NUMPAD0      : return Key.NP0;
+      case Keyboard.KEY_DECIMAL      : return Key.NP_DECIMAL;
+      case Keyboard.KEY_F11          : return Key.F11;
+      case Keyboard.KEY_F12          : return Key.F12;
+      //case Keyboard.KEY_F13          : return Key.F13;
+      //case Keyboard.KEY_F14          : return Key.F14;
+      //case Keyboard.KEY_F15          : return Key.F15;
+      //case Keyboard.KEY_F16          : return Key.F16;
+      //case Keyboard.KEY_F17          : return Key.F17;
+      //case Keyboard.KEY_F18          : return Key.F18;
+      //case Keyboard.KEY_KANA         : return Key.
+      //case Keyboard.KEY_F19          : return Key.F19;
+      //case Keyboard.KEY_CONVERT      : return Key.
+      //case Keyboard.KEY_NOCONVERT    : return Key.
+      //case Keyboard.KEY_YEN          : return Key.
+      //case Keyboard.KEY_NUMPADEQUALS : return Key.
+      case Keyboard.KEY_CIRCUMFLEX   : return Key.CIRCUMFLEX;
+      case Keyboard.KEY_AT           : return Key.AT;
+      case Keyboard.KEY_COLON        : return Key.COLON;
+      case Keyboard.KEY_UNDERLINE    : return Key.UNDERSCORE;
+      //case Keyboard.KEY_KANJI        : return Key.
+      //case Keyboard.KEY_STOP         : return Key.
+      //case Keyboard.KEY_AX           : return Key.
+      //case Keyboard.KEY_UNLABELED    : return Key.
+      //case Keyboard.KEY_NUMPADENTER  : return Key.
+      case Keyboard.KEY_RCONTROL     : return Key.CONTROL; // PlayN doesn't know left v. right
+      //case Keyboard.KEY_SECTION      : return Key.
+      //case Keyboard.KEY_NUMPADCOMMA  : return Key.
+      //case Keyboard.KEY_DIVIDE       :
+      case Keyboard.KEY_SYSRQ        : return Key.SYSRQ;
+      case Keyboard.KEY_RMENU        : return Key.ALT; // PlayN doesn't know left v. right
+      case Keyboard.KEY_FUNCTION     : return Key.FUNCTION;
+      case Keyboard.KEY_PAUSE        : return Key.PAUSE;
+      case Keyboard.KEY_HOME         : return Key.HOME;
+      case Keyboard.KEY_UP           : return Key.UP;
+      case Keyboard.KEY_PRIOR        : return Key.PAGE_UP;
+      case Keyboard.KEY_LEFT         : return Key.LEFT;
+      case Keyboard.KEY_RIGHT        : return Key.RIGHT;
+      case Keyboard.KEY_END          : return Key.END;
+      case Keyboard.KEY_DOWN         : return Key.DOWN;
+      case Keyboard.KEY_NEXT         : return Key.PAGE_DOWN;
+      case Keyboard.KEY_INSERT       : return Key.INSERT;
+      case Keyboard.KEY_DELETE       : return Key.DELETE;
+      case Keyboard.KEY_CLEAR        : return Key.CLEAR;
+      case Keyboard.KEY_LMETA        : return Key.META; // PlayN doesn't know left v. right
+      //case Keyboard.KEY_LWIN         : return Key.WINDOWS; // Duplicate with KEY_LMETA
+      case Keyboard.KEY_RMETA        : return Key.META; // PlayN doesn't know left v. right
+      //case Keyboard.KEY_RWIN         : return Key.WINDOWS; // Duplicate with KEY_RMETA
+      //case Keyboard.KEY_APPS         : return Key.
+      case Keyboard.KEY_POWER        : return Key.POWER;
+      //case Keyboard.KEY_SLEEP        : return Key.
     }
 
     return null;
