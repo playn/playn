69af5b4e173a0828e4a2d9fe1a9bbfe8a9995be5
Michael Bayne <mdb@samskivert.com>
2014-07-15 09:04:02 -0700

    Nix double close comment.

--- core/src/playn/core/Image.java
+++ core/src/playn/core/Image.java
@@ -34,7 +34,6 @@ public interface Image {
    * This image's height in pixels. When the image is loaded asynchronously, this will be 0 until
    * the image is loaded and ready for use. See {@link Image#isReady} and {@link Assets#getImage}.
    */
-   */
   float height();
 
   /**
