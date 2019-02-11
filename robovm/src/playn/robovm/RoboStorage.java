2c70b077fec1fdfa249d5339eabcec0788228fbf
Michael Bayne <mdb@samskivert.com>
2019-02-11 11:33:12 -0800

    Let subclasses get their grubby hands on our log field.

--- robovm/src/playn/robovm/RoboStorage.java
+++ robovm/src/playn/robovm/RoboStorage.java
@@ -33,8 +33,8 @@ import playn.core.Log;
 
 public class RoboStorage implements Storage {
 
-  private final Log log;
-  private final Connection conn;
+  protected final Log log;
+  protected final Connection conn;
 
   public RoboStorage(RoboPlatform platform) {
     this(platform.log(), platform.config.storageFileName);
